fda75656ad582b8162fb128d664da98844cc5035
Kennedy Rose <kennedyianrose@gmail.com>
2020-02-27 21:39:48 -0600

    Deploy

--- packages/theme/slater.config.js
+++ packages/theme/slater.config.js
@@ -1,11 +1,9 @@
-const path = require(`path`)
-
 module.exports = {
 	themes: {
 		development: {
-			id: ``,
-			password: ``,
-			store: ``,
+			id: process.env.SHOPIFY_THEME_ID,
+			password: process.env.SHOPIFY_APP_PASSWORD,
+			store: process.env.SHOPIFY_STORE_URL,
 			ignore: [
 				`settings_data.json`, // leave this here to avoid overriding theme settings on sync
 			],
