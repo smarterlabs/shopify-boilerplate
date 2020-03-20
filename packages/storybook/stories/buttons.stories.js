ce0cd9625f05961a7497bb1a60e107ba327ac7c6
Kennedy Rose <kennedyianrose@gmail.com>
2020-03-19 21:12:45 -0500

    Render liquid

--- packages/storybook/stories/buttons.stories.js
+++ packages/storybook/stories/buttons.stories.js
@@ -1,4 +1,6 @@
 import { storiesOf } from '@storybook/html'
+import { Liquid } from 'liquidjs'
+var engine = new Liquid()
 
 storiesOf(`Buttons`, module)
 	.addParameters({
@@ -8,6 +10,18 @@ storiesOf(`Buttons`, module)
 			alignItems: `center`,
 			justifyContent: `center`,
 		},
+	})
+	.add(`Liquid`, () => {
+		const el = document.createElement(`div`)
+
+		engine
+			.parseAndRender(`{{name | capitalize}}`, { name: `alice` })
+			.then(res => {
+				console.log(res)
+				el.innerHTML = res
+			})
+
+		return el
 	})
 	.add(`Default`, () => `
 		<button class='button'>Submit</button>
